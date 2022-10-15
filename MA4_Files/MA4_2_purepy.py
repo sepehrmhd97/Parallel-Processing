9066a17511e282c2087a684e89e7f19a3e5fce83
Your Name <you@example.com>
2022-10-15 22:46:45 +0200

    mod2

--- MA4_Files/MA4_2_purepy.py
+++ MA4_Files/MA4_2_purepy.py
@@ -1,1 +1,27 @@
 from time import perf_counter as pc
+import matplotlib.pyplot as plt
+
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return (fib(n-2) + fib(n-1))
+    
+if __name__ == "__main__":
+    #n = range(30,41)
+    xlist = [i for i in range(5,10)]
+    ylist = []
+    #print(xlist)
+    for i in xlist:
+        start = pc()
+        fib(i)
+        end = pc()
+        ylist.append(end - start)
+        
+    plt.plot(xlist, ylist)
+    plt.xlabel('N')
+    plt.ylabel('Time')
+    plt.show() 
+    plt.savefig('my_plot.png') 
+    
+    #print(ylist)
