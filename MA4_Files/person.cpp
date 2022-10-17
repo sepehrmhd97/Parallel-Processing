8c501b783eb630dc6a0456ddcee6a0e3453e4e02
Sepehr Mohammadi <semo.mohammadi@gmail.com>
2022-10-17 10:34:34 +0200

    3

--- MA4_Files/person.cpp
+++ MA4_Files/person.cpp
@@ -23,8 +23,8 @@ void Person::set(int n){
 	age = n;
 	}
 int Person::fib(int n){
-	if (n==1 || n== 0)
-		return n;
+	if (n==1 || n== 2)
+		return 1;
 	return fib(n-2)+fib(n-1);
 }
 
@@ -32,6 +32,7 @@ extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
 	void Person_set(Person* person, int n) {person->set(n);}
+	int Person_fib(Person* person, int n){return person->fib(n);}
 	void Person_delete(Person* person){
 		if (person){
 			delete person;
